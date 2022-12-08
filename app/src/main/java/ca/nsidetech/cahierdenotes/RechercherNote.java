53acd25c321f697201656cee0d1bc7f213c6d0c0
Nicolas <nibelleisle@osullivan-quebec.qc.ca>
2022-12-07 21:49:05 -0500

    Ajout Rechercher note + cardview pour les notes

---
+++ app/src/main/java/ca/nsidetech/cahierdenotes/RechercherNote.java
@@ -0,0 +1,14 @@
+package ca.nsidetech.cahierdenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RechercherNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rechercher_note);
+    }
+}
